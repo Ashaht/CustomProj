623e07919b7441dc12f87d2d866cb6fb2daba99f
Ashaht <E1012181526@hotmail.com>
2021-10-19 20:01:53 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,8 +15,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: function() {
+      return import ( /* webpackChunkName: "about" */ '../views/About.vue')
     }
   }
 ]
